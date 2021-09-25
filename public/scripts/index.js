4b3702af105f9060dd4eefb9cd3783eee2b76585
Nikhil Nalande <nalandenv@Nikhils-MacBook-Air.local>
2021-09-25 14:20:32 +0530

    add working slider

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,26 @@
-console.log("connected");
+const slider = document.querySelector(".slider");
+const left = document.querySelector(".btn-left");
+const right = document.querySelector(".btn-right");
+
+left.addEventListener("click", () => {
+  const transformValue = window.getComputedStyle(slider).transform;
+  const w = window.getComputedStyle(slider).width;
+  var matrix = new WebKitCSSMatrix(transformValue);
+  let currentTrans = (matrix.m41 / parseInt(w)) * 100;
+  console.log(Math.floor(currentTrans) + "%");
+  if (currentTrans == 0) {
+    currentTrans = -75;
+  }
+  slider.style.transform = `translateX(${currentTrans + 25}%)`;
+});
+right.addEventListener("click", () => {
+  const transformValue = window.getComputedStyle(slider).transform;
+  const w = window.getComputedStyle(slider).width;
+  var matrix = new WebKitCSSMatrix(transformValue);
+  let currentTrans = (matrix.m41 / parseInt(w)) * 100;
+  console.log(currentTrans);
+  if (currentTrans <= -50) {
+    currentTrans = 25;
+  }
+  slider.style.transform = `translateX(${currentTrans - 25}%)`;
+});

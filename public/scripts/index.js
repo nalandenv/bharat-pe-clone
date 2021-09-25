d61402e70cdcf08710ad509066b26d2f4c859e6c
Nikhil Nalande <nalandenv@Nikhils-MacBook-Air.local>
2021-09-25 15:38:52 +0530

    complete slider with dynamic style and all content

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,38 @@
 const slider = document.querySelector(".slider");
 const left = document.querySelector(".btn-left");
 const right = document.querySelector(".btn-right");
-
+const whatsIn = document.querySelector("#whats-in");
+let slideNum = 1;
 left.addEventListener("click", () => {
-  const transformValue = window.getComputedStyle(slider).transform;
-  const w = window.getComputedStyle(slider).width;
-  var matrix = new WebKitCSSMatrix(transformValue);
-  let currentTrans = (matrix.m41 / parseInt(w)) * 100;
-  console.log(Math.floor(currentTrans) + "%");
-  if (currentTrans == 0) {
-    currentTrans = -75;
-  }
-  slider.style.transform = `translateX(${currentTrans + 25}%)`;
+  changeSlide(0, -75, 4, -25);
 });
 right.addEventListener("click", () => {
+  changeSlide(-50, 25, 0, 25);
+});
+
+const changeSlide = (direction, trans, slideN, val) => {
   const transformValue = window.getComputedStyle(slider).transform;
   const w = window.getComputedStyle(slider).width;
   var matrix = new WebKitCSSMatrix(transformValue);
   let currentTrans = (matrix.m41 / parseInt(w)) * 100;
-  console.log(currentTrans);
-  if (currentTrans <= -50) {
-    currentTrans = 25;
+  if (currentTrans == direction) {
+    currentTrans = trans;
+    slideNum = slideN;
   }
-  slider.style.transform = `translateX(${currentTrans - 25}%)`;
-});
+  const dir = direction;
+  if (dir == -50) {
+    slideNum++;
+  } else {
+    slideNum--;
+  }
+  console.log(slideNum);
+  if (slideNum == 1) {
+    whatsIn.style.color = "#444";
+    console.log("black");
+  } else {
+    whatsIn.style.color = "#e0e0e0";
+    console.log("white");
+  }
+  console.log({ slideNum });
+  slider.style.transform = `translateX(${currentTrans - val}%)`;
+};
